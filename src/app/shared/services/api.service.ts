750d788260ec3016e2c8f54ee40a8a59acc859c9
subhransuchin2 <33114981+subhransuchin2@users.noreply.github.com>
2020-05-24 00:21:56 +0530

    Add files via upload

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,42 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { TokenRefreshService } from '../token-refresh/token-refresh.service';
-// export declare class DataFetcher {
-//     private http;
-//     private tokenRefreshService;
-//     constructor(http: HttpClient, tokenRefreshService: TokenRefreshService);
-//     fetch(url: string, lang: string, opts?: any, body?: any, cacheBustingValue?: string): Observable<any>;
-// }
 
-
-// import {Injectable} from '@angular/core';
-// import {LanguageService, UserInfoService} from '@hdca/fe-angular-shared';
-// import {DataSender, OriginService} from '@hdca/fe-angular-core';
-// import {myAccountApiUrls} from '../constants/api.constants';
-// @Injectable()
-// export class OPayService {
-//     constructor(private userInfo: UserInfoService, private dataSender: DataSender, private originService: OriginService, private languageService: LanguageService) {
-//     }
-//     getOPToken() {
-//         const email = this.userInfo.getUserAccountData().email;
-//         const cartId = this.userInfo.getCookie('cart');
-//         const url = myAccountApiUrls.BASE_URL(this.originService.getOrigin()) + myAccountApiUrls.OPAY_TOKEN(email, cartId);
-//         const lang = this.languageService.getLanguage();
-//         return this.dataSender.post(url, lang);
-//     }
-// }
-
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ApiService {
-
-//   constructor(private httpClient: HttpClient) { }
-// }
 @Injectable({
   providedIn: 'root'
 })
